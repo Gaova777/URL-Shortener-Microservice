bf009490dad258d1b74347750f58e920f3215557
Gaova777 <gaova777@utp.edu.co>
2023-01-18 14:24:19 -0500

    Master and Deploy

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({extended:true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,6 +21,51 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+const originalUrls = [];
+const shortUrl = [];
+
+app.post('/api/shorturl', (req, res)=>{
+  const url = req.body.url;
+  const foundIndex = originalUrls.indexOf(url);
+
+  if(!url.includes("https://") && !url.includes("http://")){
+    return res.json({ error: 'invalid url'})
+  }
+
+  if(foundIndex<0){
+    originalUrls.push(url);
+    shortUrl.push(shortUrl.length)
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrl.length-1
+    })
+  }
+
+
+
+return res.json({
+  original_url: url,
+  short_url: shortUrl[foundIndex]
+})
+})
+
+
+app.get("/api/shorturl/:shorturl", (req,res)=>{
+  const shorturl = parseInt(req.params.shorturl);
+
+
+  const foundIndex = shortUrl.indexOf(shorturl)
+
+  if(foundIndex < 0){
+    return res.json({
+      "error": "No short URL found for the given input"
+    })
+  }
+
+  res.redirect(originalUrls[foundIndex])
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
